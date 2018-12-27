2ebff285ab61ff570c75fd2df419a292056da99d
midori <pigsdofly6@gmail.com>
2018-12-27 19:17:38 +0000

    a little bit of refactoring and step 4/start of 5

--- js/script.js
+++ js/script.js
@@ -2,7 +2,12 @@ var counter = 1;
 var radius = 20;
 //interval variable for animations
 var interval;
-var anim_iter = 2;
+var animIter = 2;
+var explanationY = 240;
+var lineSize = 17; //16px text size + 1
+var aliceX = 70;
+var bobX = 370;
+var defaultY = 70;
 
 function draw_circle(context, centerX, centerY, rad, colour) {
 //function to reduce the amount of boilerplate code needed to draw a single circle
@@ -17,19 +22,45 @@ function draw_circle(context, centerX, centerY, rad, colour) {
 
 function draw_base(context, line) {
 //draws the base image of alice/bob and the message line between them
-    var textY = radius + 90;
-    draw_circle(context, 70, 70, radius, 'black');
-    draw_circle(context, 370, 70, radius, 'black');
+    var textY = 40;
+    draw_circle(context, aliceX, defaultY, radius, 'black');
+    draw_circle(context, bobX, defaultY, radius, 'black');
     
-    context.fillText("Alice", 45, textY);
-    context.fillText("Bob", 350, textY);
+    context.fillText("Alice", aliceX - 25, textY);
+    context.fillText("Bob", bobX - 20, textY);
     
 	if(line) {
 		context.beginPath();
-		context.moveTo(70, 70);
-		context.lineTo(370, 70);
+		context.moveTo(aliceX, defaultY);
+		context.lineTo(bobX, defaultY);
 		context.stroke();
 	}
+}
+
+function draw_animated_encryption(context, i) {
+//animation for drawing line going through keys to encrypt and decrypt
+    var leftBound = aliceX + 70;
+    var rightBound = bobX - 70;
+    if(i == leftBound || i == rightBound) {
+        draw_circle(context, i, defaultY, radius * 0.8, 'blue');
+        if(i == leftBound)
+            context.fillText("Encrypt", i-radius, radius+85);
+        else if(i == rightBound)
+            context.fillText("Decrypt", i-radius, radius+85);
+    }	
+
+    context.beginPath();
+    context.moveTo(i, defaultY);
+    if( i < leftBound || i >= (rightBound + radius) - animIter )
+        context.strokeStyle = 'black';
+    else
+        context.strokeStyle = 'blue';
+
+    i += animIter;
+
+    context.lineTo(i, defaultY);
+    context.stroke();
+    return i;
 }
 
 function draw_steps(context) {
@@ -38,33 +69,34 @@ function draw_steps(context) {
         case 1:
             //first screen
             draw_base(context, true);
-    		context.fillText("Message", 181, 60);
+    		context.fillText("Message", aliceX + 100, 40);
             
-            context.fillText("Alice and Bob want to send a message to each other.", 20, 230);
+            context.fillText("Alice and Bob want to send a message to each other.", 20, explanationY);
             
             break;
         case 2:
             draw_base(context, true);
-    		context.fillText("Message", 181, 60);
+    		context.fillText("Message", aliceX + 100, 40);
 
-            var trudyX = (70 + 370) / 2;
+            var trudyX = (aliceX + bobX) / 2;
             context.beginPath();
             context.strokeStyle = 'red';
-            context.moveTo(trudyX, 70);
-            var i = 70;
+            context.moveTo(trudyX, defaultY);
+            var i = aliceX;
+            var trudyY = defaultY + 100;
             interval = window.setInterval(function() {
-                if(i != 170) {
-                    i+= anim_iter;
+                if(i != (trudyY)) {
+                    i+= animIter;
                     context.lineTo(trudyX, i);
                     context.stroke();
                 } else {
-                    draw_circle(context, trudyX, 170, radius, 'red');
+                    draw_circle(context, trudyX, trudyY, radius, 'red');
                     window.clearInterval(interval);
                     context.fillText("Trudy", trudyX-radius, 210);
                     
                     context.fillStyle = 'black';
-                    context.fillText("But when their connection is unencrypted,", 20, 240);
-                    context.fillText("Trudy can listen in.", 20, 257);
+                    context.fillText("But when their connection is unencrypted,", 20, explanationY);
+                    context.fillText("Trudy can listen in.", 20, explanationY+lineSize);
                     
                 }
             }, 15);
@@ -72,34 +104,21 @@ function draw_steps(context) {
         case 3:
 			draw_base(context, false);
 			var i = 56;
+            c3Y = explanationY;
+            context.fillText("To stop their messages being tampered with, Alice and Bob can use symmetric encryption.", 20, c3Y);
+            context.fillText("In symmetric encryption, both parties use the same key to encrypt and decrypt the message.", 20, c3Y+lineSize);
+            c3Y += (lineSize*2);
+            
 			interval = window.setInterval(function() {
-				if(i == 140 || i == 300) {
-					draw_circle(context, i, 70, radius * 0.8, 'blue');
-					context.fillText("Key", i-20, radius+85);
-				}	
-
-				if( i >= 370) {
+                i = draw_animated_encryption(context, i);
+				if( i >= bobX) {
 					window.clearInterval(interval);
+                    context.fillStyle = 'blue';
+                    context.fillText("Encrypted Message", 140, 40);
 					context.fillStyle = 'black';
-					context.fillText("One method of circumventing this is symmetric encryption.", 20, 240);
-					context.fillText("But this requires both parties to have the same key.", 20, 257);
-					context.fillText("How do they share a key without already knowing it?", 20, 274);
-				} else if( i < 140 || i >= 318 ) {
-					context.beginPath();
-					context.moveTo(i, 70);
-					context.strokeStyle = 'black';
-					i += anim_iter;
 					
-					context.lineTo(i, 70);
-					context.stroke();
-				} else {
-					context.beginPath();
-					context.moveTo(i, 70);
-					context.strokeStyle = 'blue';
-					i += anim_iter;
-					
-					context.lineTo(i, 70);
-					context.stroke();
+					context.fillText("But this requires both parties to have the same key.", 20, c3Y+lineSize);
+					context.fillText("How do they share a key without already knowing it?", 20, c3Y+(lineSize*2));
 				}
 			}, 10);
 			
@@ -109,10 +128,86 @@ function draw_steps(context) {
             //message -> key -> ciphertext animation
             //but if both parties don't have a key, how do they share?
         case 4:
-            //One way is a private channel for key sharing that only Alice and Bob know
+            draw_base(context, false);
+            context.fillText("One way past what is known as the Key Distribution Problem is to", 20, explanationY);
+            context.fillText("share the key through a known secure channel beforehand (such as a physical courier).", 20, explanationY+lineSize);
+            var x = i = aliceX;
+            var y = defaultY;
+            var secure_channel = false;
+            context.moveTo(aliceX,defaultY);
+            interval = window.setInterval(function() {
+				if(secure_channel) {
+                    context.lineWidth = 5;
+                    i = draw_animated_encryption(context, i);
+                    if(i >= bobX) {
+                        window.clearInterval(interval);
+                        context.fillStyle = "black";
+                        context.fillText("But this solution isn't practical for most interactions, especially on the scale of the internet.", 20, explanationY+(lineSize*3));
+                        context.fillText("And so we must use other methods, like Asymmetric Encryption.", 20, explanationY+(lineSize*4));
+                    }
+                } else {
+                    //animated drawing of dotted line
+                    if(x == bobX) {
+                        if(y == defaultY) {
+                            secure_channel = true;
+                            context.fillText("Key shared over secure channel", 100, 190);
+                            //drawing an arrow back to (70,70)
+                            context.lineWidth = 2;
+                            context.moveTo(bobX, 160);
+                            context.lineTo(aliceX, 160);
+                            context.stroke();
+                            context.beginPath();
+                            context.moveTo(aliceX, 160);
+                            context.lineTo(80, 170);
+                            context.lineTo(80, 150);
+                            context.fill();
+                        } else {
+                            y -= animIter;
+                            context.lineTo(x, y);
+                        }
+                    }
+                    else if(y < 140) {
+                        y += animIter;
+                        context.lineTo(aliceX, y);
+                    } else {
+                        var gap = ((x - aliceX) % 20) == 0; //evaluates as a true or false for if we draw a gap
+                        if(!gap || x == aliceX) {
+                            x += animIter;
+                            context.lineTo(x, 140);
+                            context.stroke();
+                        } else {
+                            x += 10;
+                            context.moveTo(x, 140);
+                        }
+                    }
+                    context.stroke();
+                }
+            }, 10);
+            //One way is a private channel for key sharing that only Alice and Bob know about
             //private channel -> message -> key -> ciphertext
+			//But for anonymous interactions on the scale of the internet, this solution is unfeasible
 			break;
         case 5:
+            //draw_base(context, false);
+            var messageY = 20;
+            context.fillText("In Asymmetric Encryption, everyone has two personal keys.",20,messageY);
+            context.fillText("In the RSA algorithm, these keys are created from three variables: e, p and q. ",20,messageY+lineSize);
+            
+            var midX = (aliceX+bobX)/2;
+
+            draw_circle(context, midX, defaultY, radius, "black");
+            context.fillText("Alice", midX+40, defaultY+5);
+            
+
+
+            //Generates two numbers, p and q
+            break;
+        case 6:
+            //For Asymmetric Encryption to work, both sides have to first generate two keys
+            //draw key generation example
+			//Do smaller animations bit-by-bit to explain
+			//Each person has two keys, a public key for encryption and a private key for decryption. 
+            
             break;
     }
 }
